5c922e16abb02e47b553f8632c94622154b690f6
Corey Mays <corey.mays17@gmail.com>
2024-09-19 18:47:40 -0400

    final commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args)
     {
+        double bal = 5000;
+        final double INTEREST_RATE = .17;
 
+        bal = (bal * INTEREST_RATE) + bal;
+        System.out.println("The balance after one month will be " + bal);
+        bal = (bal * INTEREST_RATE) + bal;
+        System.out.println("The balance after two months will be " + bal);
     }
 }
